e9a3b6da3db2809a06264d595c8e781019952673
Darren Reid <darren.j.reid@gmail.com>
2016-06-13 13:10:18 +1000

    Try fix jspm

--- DtoGen/DtoGen/DtoGen/config.js
+++ DtoGen/DtoGen/DtoGen/config.js
@@ -8,11 +8,13 @@ System.config({
   },
 
   map: {
+    "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.3.6",
     "aurelia-bootstrapper": "npm:aurelia-bootstrapper@1.0.0-beta.1.2.1",
     "aurelia-framework": "npm:aurelia-framework@1.0.0-beta.1.2.5",
     "aurelia-http-client": "npm:aurelia-http-client@1.0.0-beta.1.2.2",
     "aurelia-logging-console": "npm:aurelia-logging-console@1.0.0-beta.1.2.2",
     "aurelia-router": "npm:aurelia-router@1.0.0-beta.1.2.4",
+    "aurelia-templating": "npm:aurelia-templating@1.0.0-beta.1.2.7",
     "aurelia-templating-binding": "npm:aurelia-templating-binding@1.0.0-beta.1.2.4",
     "aurelia-templating-resources": "npm:aurelia-templating-resources@1.0.0-beta.1.2.6",
     "aurelia-templating-router": "npm:aurelia-templating-router@1.0.0-beta.1.2.1",
